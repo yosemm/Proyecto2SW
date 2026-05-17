b2afdb9766a49d68ee48815f686aa1680f1404c0
yosemm <140467686+yosemm@users.noreply.github.com>
2026-05-16 20:51:58 -0600

    feat: limpia plantilla y agrega el patrón lazy initializer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [items, setItems] = useState(
+    () => JSON.parse(localStorage.getItem('items') || '[]')
+  );
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
   return (
     <>
-      
+      <div>
+        <h1>Backlog de Videojuegos</h1>
+        <span>Total de juegos: {items.length}</span>
+      </div>
     </>
   )
 }
